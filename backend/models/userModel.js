87f0a404f1be82b5ca1a322e55a0135bc8153a9a
astrostar41ust <astrostar41ust@gmail.com>
2025-03-03 17:39:39 +0700

    update database schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  weight: { type: Number, default: 0 },
+  height: { type: Number, default: 0 },
+  fat: { type: Number, default: 0 },
+});
+
+
+
+const userModel = mongoose.model("userModel", userSchema);
+
+module.exports = userModel;
